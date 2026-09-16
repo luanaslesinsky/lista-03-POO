6622ef7eb08f01c5554a87ae6a957411d28363cc
luana <luanyluaky@gmail.com>
2026-09-15 23:25:23 -0300

    Add files via upload

---
+++ meu-primeiro-sistema/src/br/com/meusistema/model/Carro.java
@@ -0,0 +1,11 @@
+package br.com.meusistema.model;
+
+public class Carro {
+    public String modelo;
+    public String cor;
+
+    public void buzinar(){
+        System.out.println("O " + modelo + " " + cor + " fez: BIII BIII!");
+    }
+}
+    
